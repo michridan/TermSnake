bea6b3cd24b422540cdbdd0651a270fba83fe1e7
Michael Dandrea <michael.dandrea@wsu.edu>
2017-10-13 13:56:25 -0700

    Edit main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*	TermSnake: A game by Michael Dandrea
  *	Last Updated: 10/11/17
  *	Description: This game will ultimately be a version of the classic Snake game to be played in terminal
-*/
+ */
 
 #include <ncurses.h>
 
@@ -10,7 +10,8 @@ int main(void)
 	WINDOW *board;
 	int inity, initx;
 
-	getmaxyx
+	initscr(); 
+	cbreak();
 
 	return 0;
 }
